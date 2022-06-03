47209a7650fec5616fa1cfa6aa755148717c1b69
A.Troeglazova <shyryo4ik03@gmail.com>
2022-06-03 21:29:17 +0700

    Создала функцию табличной шифровки.
    Сделала функцию табличной шифровки и местоположение значения в пределах вектора.

---
+++ source/TablShifr.cpp
@@ -0,0 +1,40 @@
+#include "TablShifr.h"
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iterator>
+std::pair<bool, int> findInVector(const std::vector<char>& Case, const char& element)
+{
+    std::pair<bool, int > result;
+    // Find given element in vector
+    auto it = std::find(Case.begin(), Case.end(), element);
+    if (it != Case.end())
+    {
+        result.second = distance(Case.begin(), it);
+        result.first = true;
+    }
+    else
+    {
+        result.first = false;
+        result.second = -1;
+    }
+    return result;
+}
+std::string Tabl(std::string S1)
+{
+	std::vector<char>Case = { ' ' , 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	std::vector<char>Symbol = { ' ', '+','!','@','#','$','%','^','&','*','[',']',')','(','=','`',':',';','{','}','.',',','<','>','"','?','/','\'' };
+    std::string S2;
+	for (int i = 0; i < S1.length(); i++) {
+        std::pair<bool, int> result = findInVector(Case, S1[i]);
+        if (result.first) {
+            S2.push_back(Symbol[result.second]);
+        }
+        else {
+            S2.push_back(S1[i]);
+        }
+		
+	}
+  
+    return S2;
+}
---
+++ source/TablShifr.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iterator>
+
+std::pair<bool, int > findInVector(const std::vector<char>& Case, const char& element);
+std::string Tabl(std::string S1);
+
