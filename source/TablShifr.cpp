44ae8bd978102fe6b97961fade6ec6eedeb80f4a
A.Troeglazova <shyryo4ik03@gmail.com>
2022-06-04 00:00:17 +0700

    Была добавлена функция дешифрование.
    Была добавлена функция дешифрование.

--- source/TablShifr.cpp
+++ source/TablShifr.cpp
@@ -20,6 +20,25 @@ std::pair<bool, int> findInVector(const std::vector<char>& Case, const char& ele
     }
     return result;
 }
+std::string DecShif(std::string S3)
+{
+    std::vector<char>Case = { ' ' , 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	std::vector<char>Symbol = { ' ', '+','!','@','#','$','%','^','&','*','[',']',')','(','=','`',':',';','{','}','.',',','<','>','"','?','/','\'' };
+    std::string S2;
+	for (int i = 0; i < S3.length(); i++) {
+        std::pair<bool, int> result = findInVector(Symbol, S3[i]);
+        if (result.first) {
+            S2.push_back(Case[result.second]);
+        }
+        else {
+            S2.push_back(S3[i]);
+        }
+		
+	}
+  
+    return S2;
+
+}
 std::string Tabl(std::string S1)
 {
 	std::vector<char>Case = { ' ' , 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
--- source/TablShifr.h
+++ source/TablShifr.h
@@ -6,5 +6,6 @@
 #include <iterator>
 
 std::pair<bool, int > findInVector(const std::vector<char>& Case, const char& element);
+std::string DecShif(std::string S3);
 std::string Tabl(std::string S1);
 
