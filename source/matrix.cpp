4b869d1eb56f6f7ea98dda611a0d63ce54326912
Ivan <SlenderNSK@gmail.com>
2022-06-03 13:42:32 +0700

    added an algorithm for full squear length of string
    added an algorithm for full squear length of string for example 25. added check to pick wich algorithm to use, full square or not full square

--- source/matrix.cpp
+++ source/matrix.cpp
@@ -1,43 +1,73 @@
 #include "matrix.h"
 
-void matrix(std::string* text, int size)
+std::vector<std::string> matrix(std::string* text, int size)
 {
+
 	std::string text_cont = *text;
 	std::string temp_str;
 	std::vector<std::string> matrix;
 	int i, len, j, k;
 	j = k = 0;
 	len = text->length();
-	for (i = 0; i <= len; i += size) {
-		k = 0;
-		while (k < size) {
+	if (ceil(sqrt(text_cont.length())) == sqrt(text_cont.length()))
+	{
+		for (i = 0; i <= len; i += size) {
+			k = 0;
+			while (k < size) {
+				if (text_cont[j] != '\0') {
+					temp_str.push_back(text_cont[j]);
+					j++;
+					k++;
+				}
+				else {
+					break;
+				}
+			}
 			if (text_cont[j] != '\0') {
-				temp_str.push_back(text_cont[j]);
-				j++;
-				k++;
+				matrix.push_back(temp_str);
+				temp_str.clear();
 			}
 			else {
+				matrix.push_back(temp_str);
+				temp_str.clear();
 				break;
 			}
 		}
-		if (text_cont[j] != '\0') {
-			matrix.push_back(temp_str);
-			temp_str.clear();
-		}
-		else {
-			while (j < size*size) {
-				while (k < size) {
-					temp_str.push_back(' ');
-					k++;
+	}
+	else {
+		for (i = 0; i <= len; i += size) {
+			k = 0;
+			while (k < size) {
+				if (text_cont[j] != '\0') {
+					temp_str.push_back(text_cont[j]);
 					j++;
+					k++;
 				}
+				else {
+					break;
+				}
+			}
+			if (text_cont[j] != '\0') {
 				matrix.push_back(temp_str);
 				temp_str.clear();
-				k = 0;
 			}
-			break;
+			else {
+				while (j < size * size) {
+					while (k < size) {
+						temp_str.push_back(' ');
+						k++;
+						j++;
+					}
+					matrix.push_back(temp_str);
+					temp_str.clear();
+					k = 0;
+				}
+				break;
+			}
 		}
 	}
+	std::cout << matrix[0][0] << matrix[3][3];
+	return matrix;
 }
 
 
@@ -49,3 +79,4 @@ int size_of_matrix(std::string text)
 	}
 	return sqrt(text.length());
 }
+
